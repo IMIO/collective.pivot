4e84e9fdc99d658350e9cba6f1e2cd674b4fd963
Christophe Boulanger <christophe.boulanger@imio.be>
2020-10-06 09:58:03 +0200

    Add new field "ws_url" in registry

--- src/collective/pivot/interfaces.py
+++ src/collective/pivot/interfaces.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from collective.pivot import _
+from plone import schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.interface import Interface
 
 
 class ICollectivePivotLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class ICollectivePivotSettings(Interface):
+
+    ws_url = schema.TextLine(title=_(u"Webservice url"), required=True)
