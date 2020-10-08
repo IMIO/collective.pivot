24af59ba9ba9cc1e9daad1fa542993f67647e512
Christophe Boulanger <christophe.boulanger@imio.be>
2020-10-08 15:17:50 +0200

    Interface is now in control panel.

--- src/collective/pivot/interfaces.py
+++ src/collective/pivot/interfaces.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from collective.pivot import _
-from plone import schema
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-from zope.interface import Interface
 
 
 class ICollectivePivotLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
-
-
-class ICollectivePivotSettings(Interface):
-
-    ws_url = schema.TextLine(title=_(u"Webservice url"), required=True)
-    ws_key = schema.TextLine(title=_(u"Webservice (header) key"), required=True)
