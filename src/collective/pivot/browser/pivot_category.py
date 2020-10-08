57db48dfea2c6d371c37604e4737c1b509f3c819
Christophe Boulanger <christophe.boulanger@imio.be>
2020-10-08 15:19:42 +0200

    Update browserview

--- src/collective/pivot/browser/pivot_category.py
+++ src/collective/pivot/browser/pivot_category.py
@@ -1,5 +1,8 @@
-from plone import api
+# -*- coding: utf-8 -*-
+from collective.pivot.browser.controlpanel import IPivotSettings
+from plone.registry.interfaces import IRegistry
 from Products.Five import BrowserView
+from zope.component import getUtility
 
 
 class PivotCategoryView(BrowserView):
@@ -7,21 +10,48 @@ class PivotCategoryView(BrowserView):
     """
 
     @property
-    def full_url_query(self):
-        return "{}{}".format(self.ws_url, self.context.category_id)
+    def settings(self):
+        registry = getUtility(IRegistry)
+        return registry.forInterface(IPivotSettings)
+
+    @property
+    def query_url(self):
+        cp = "cp:{}".format("|".join(self.zip_codes))
+        return "{}query/{};content=1;pretty=true;fmt=json;param={}".format(
+            self.ws_url, self.context.category_id, cp
+        )
+
+    @property
+    def thesaurus_url(self):
+        """
+        https://pivotweb.tourismewallonie.be/PivotWeb-3.1/thesaurus/family/urn:fam:1;pretty=true;fmt=json
+        """
+        cp = "cp:{}".format("|".join(self.zip_codes))
+        return "{}thesaurus/family/{};content=1;pretty=true;fmt=json;param={}".format(
+            self.ws_url, self.context.category_id, cp
+        )
 
     @property
     def ws_url(self):
-        return api.portal.get_registry_record(
-            "collective.pivot.interfaces.ICollectivePivotSettings.ws_url"
-        )
+        return self.settings.ws_url
+
+    @property
+    def zip_codes(self):
+        if self.context.zip_codes is not None:
+            return self.context.zip_codes
+        else:
+            return self.settings.zip_codes
 
     @property
     def ws_key(self):
-        return api.portal.get_registry_record(
-            "collective.pivot.interfaces.ICollectivePivotSettings.ws_key"
-        )
+        return self.settings.ws_key
 
-
-# ;content=1;fmt=json' \
-# --header 'ws_key: 1b24b1ce-3c3f-405f-8881-56721a0017b3'
+    @property
+    def test_config(self):
+        if self.zip_codes is None:
+            return False
+        if self.ws_url is None:
+            return False
+        if self.ws_key is None:
+            return False
+        return True
