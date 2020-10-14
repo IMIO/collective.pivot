fa1dfd7b9ab982a987ac3f75e63ba533ee8a30af
Christophe Boulanger <christophe.boulanger@imio.be>
2020-10-14 13:53:19 +0200

    blacked file

--- src/collective/pivot/content/pivot_category.py
+++ src/collective/pivot/content/pivot_category.py
@@ -14,7 +14,9 @@ class IPivotCategory(model.Schema):
     category_id = schema.TextLine(title=_(u"Category id"), required=True)
     zip_codes = schema.List(
         title=_(u"Zip codes"),
-        description=_(u"Leave empty if you want to get zip codes from global settings."),
+        description=_(
+            u"Leave empty if you want to get zip codes from global settings."
+        ),
         value_type=schema.TextLine(title=_(u"Zip code")),
         required=False,
     )
