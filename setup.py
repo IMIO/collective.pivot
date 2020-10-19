22004bedc96f1f98c5ee701b66418d67357506b0
Christophe Boulanger <christophe.boulanger@imio.be>
2020-10-19 09:11:49 +0200

    Add requests-mock for tests

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
             # plone_coredev tests as of 2016-04-01.
             'plone.testing>=5.0.0',
             'plone.app.robotframework[debug]',
+            'requests-mock',
         ],
     },
     entry_points="""
