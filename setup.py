0b57a165c3af6c5d56785a631ff72764181acccf
Christophe Boulanger <christophe.boulanger@imio.be>
2021-02-18 10:54:01 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -23,8 +23,10 @@ setup(
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
         "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -44,7 +46,6 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
@@ -53,7 +54,6 @@ setup(
         'plone.api>=1.8.4',
         'plone.restapi',
         'plone.app.dexterity',
-        'plone.app.referenceablebehavior',
         'plone.app.relationfield',
         'plone.app.lockingbehavior',
         'plone.schema',
