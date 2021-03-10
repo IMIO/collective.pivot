3bf32c9537062ade8e8e31844b3259f37830138f
Christophe Boulanger <christophe.boulanger@imio.be>
2021-03-10 16:12:11 +0100

    Back to development: 1.0a7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.pivot",
-    version="1.0a6",
+    version="1.0a7.dev0",
     description="Display CGT Pivot informations",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
