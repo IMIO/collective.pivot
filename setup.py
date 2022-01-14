fef6b616503af2751eaf2a5b81fd078451671ee8
Laurent Lasudry <lasudry@gmail.com>
2022-01-14 10:41:46 +0100

    Back to development: 1.0a8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.pivot",
-    version="1.0a7",
+    version="1.0a8.dev0",
     description="Display CGT Pivot informations",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
